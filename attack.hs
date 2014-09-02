d4b61d9d5cfaddf4670afefdb244bd8a5dd9e1e1
Nick Pierson <nkcrpn@gmail.com>
2014-09-01 21:54:26 -0500

    Adds function to perform a shift cipher

--- attack.hs
+++ attack.hs
@@ -1,8 +1,9 @@
 import System.IO
+import Data.Char
 
 main = do
     cipher <- prompt "Enter cipher: "
-    let (shift, plaintext) = decipher cipher
+    let (shift, plaintext) = attack cipher
     putStrLn ("\nShift: " ++ show shift)
     putStrLn ("Plaintext: " ++ plaintext)
 
@@ -12,5 +13,17 @@ prompt text = do
     hFlush stdout
     getLine
 
-decipher :: String -> (Int, String)
-decipher cipher = (0, cipher)
+attack :: String -> (Int, String)
+attack cipher = (0, cipher)
+
+encipher :: Int -> String -> String
+encipher shift [] = []
+encipher shift (c:cs) = shiftChar shift c : encipher shift cs
+
+shiftChar :: Int -> Char -> Char
+shiftChar shift c
+    | isSpace c = c
+    | otherwise = let shifted = ord c + shift
+                      -- Assume lowercase
+                      newOrd = (shifted - ord 'a') `mod` 26 + ord 'a'
+                  in  chr newOrd

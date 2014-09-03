7a8b22047731e3c373ed9b50880d6a6ecd9772b9
Nick Pierson <nkcrpn@gmail.com>
2014-09-03 01:22:25 -0500

    Deciphering shifted text is now possible. Logic currently still mixed with IO :(

--- attack.hs
+++ attack.hs
@@ -1,11 +1,17 @@
 import System.IO
+import Control.Monad
+import Data.List
 import Data.Char
+import Database.HDBC
+import Database.HDBC.Sqlite3
 
 main = do
     cipher <- prompt "Enter cipher: "
-    let (shift, plaintext) = attack cipher
+    let allShiftedTexts = getAllShiftedTexts cipher
+    likelyText <- getLikelyText allShiftedTexts
+    let shift = getShift cipher likelyText
     putStrLn ("\nShift: " ++ show shift)
-    putStrLn ("Plaintext: " ++ plaintext)
+    putStrLn ("Plaintext: " ++ likelyText)
 
 prompt :: String -> IO String
 prompt text = do
@@ -13,9 +19,40 @@ prompt text = do
     hFlush stdout
     getLine
 
-attack :: String -> (Int, String)
-attack cipher = (0, cipher)
+getAllShiftedTexts :: String -> [String]
+getAllShiftedTexts cipher = [encipher x cipher | x <- [1..26]]
 
+getLikelyText :: [String] -> IO String
+getLikelyText allTexts = do
+    let realWordsPerText = map numRealWords allTexts
+    maxRealWords <- liftM maximum $ sequence realWordsPerText
+    maybeIndex <- (liftM $ elemIndex maxRealWords) $ sequence realWordsPerText
+    let index = case maybeIndex of
+                    Just x -> x
+                    Nothing -> 0
+    return (allTexts !! index)
+
+numRealWords :: String -> IO Int
+numRealWords text = do
+    let splitText = words text
+        filtered = filterM exists splitText
+    liftM length $ filtered
+
+-- Database functions
+exists :: String -> IO Bool
+exists word = do
+    conn <- connectSqlite3 "words.db"
+    let statement = "SELECT EXISTS(SELECT 1 FROM words WHERE word=?)"
+    results <- quickQuery' conn statement [toSql word]
+    let result = head $ head results
+        wordExists = (fromSql result :: Int) == 1
+    disconnect conn
+    return wordExists
+
+getShift :: String -> String -> Int
+getShift source target = 1
+
+-- Shift cipher implementation
 encipher :: Int -> String -> String
 encipher shift [] = []
 encipher shift (c:cs) = shiftChar shift c : encipher shift cs

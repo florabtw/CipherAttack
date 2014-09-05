35581b3317e73822a12e61a77eda7458ed9bf667
Nick Pierson <nkcrpn@gmail.com>
2014-09-04 20:27:31 -0500

    Implements getShift. Changes to connect to DB only once.

--- attack.hs
+++ attack.hs
@@ -24,33 +24,33 @@ getAllShiftedTexts cipher = [encipher x cipher | x <- [1..26]]
 
 getLikelyText :: [String] -> IO String
 getLikelyText allTexts = do
-    let realWordsPerText = map numRealWords allTexts
+    conn <- connectSqlite3 "words.db"
+    let realWordsPerText = map (numRealWords conn) allTexts
     maxRealWords <- liftM maximum $ sequence realWordsPerText
-    maybeIndex <- (liftM $ elemIndex maxRealWords) $ sequence realWordsPerText
+    maybeIndex <- liftM (elemIndex maxRealWords) $ sequence realWordsPerText
     let index = case maybeIndex of
                     Just x -> x
                     Nothing -> 0
+    disconnect conn
     return (allTexts !! index)
 
-numRealWords :: String -> IO Int
-numRealWords text = do
-    let splitText = words text
-        filtered = filterM exists splitText
-    liftM length $ filtered
+numRealWords :: Connection -> String -> IO Int
+numRealWords conn = liftM length . filterM (exists conn) . words
 
--- Database functions
-exists :: String -> IO Bool
-exists word = do
-    conn <- connectSqlite3 "words.db"
-    let statement = "SELECT EXISTS(SELECT 1 FROM words WHERE word=?)"
-    results <- quickQuery' conn statement [toSql word]
+exists :: Connection -> String -> IO Bool
+exists conn word = do
+    results <- quickQuery' conn sqlExists [toSql word]
     let result = head $ head results
         wordExists = (fromSql result :: Int) == 1
-    disconnect conn
     return wordExists
 
+sqlExists :: String
+sqlExists = "SELECT EXISTS(SELECT 1 FROM words WHERE word=?)"
+
 getShift :: String -> String -> Int
-getShift source target = 1
+getShift source target =
+    let firstChar = ord . head
+    in  abs $ firstChar source - firstChar target
 
 -- Shift cipher implementation
 encipher :: Int -> String -> String
